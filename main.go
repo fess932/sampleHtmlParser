6eb359598f8a043e0d501061d1919e00cb9677e9
fess932 <fess932@gmail.com>
2021-06-25 15:01:41 +0300

    git ignore

--- main.go
+++ main.go
@@ -59,15 +59,19 @@ const (
 func (p *Parser) parse2() {
 	var err error
 	var s byte
+	defer log.Println("title: ", p.title)
 
 	for {
 		s, err = p.r.ReadByte()
 		if err != nil {
-			log.Fatalln(err)
+			log.Println(err)
+			return
 		}
 
+		//log.Println("in title", p.inTitle)
+
 		if p.inTitle {
-			p.writeTitle()
+			p.writeTitle(s)
 		}
 
 		// ищем открывающий тег если тег не открыт
@@ -77,17 +81,25 @@ func (p *Parser) parse2() {
 		}
 		// если тег открыт, если текущий символ закрывающий тег то закрываем тег, ичищаем буфер переходим на следующую итерацию
 		if p.findCloseTag(s) {
+			if p.inTitle {
+				p.inTitle = false // при закрывающем title выходим из title
+				p.title = p.buf.String()
+			}
 			log.Printf("{%s}", p.buf.String())
 			p.buf.Reset()
 			continue
 		}
 
 		if strings.Contains(p.buf.String(), "title") {
-
+			p.inTitle = true
 		}
 
 		p.buf.WriteByte(s)
 	}
+}
+
+func (p *Parser) writeTitle(b byte) {
+	p.buf.WriteByte(b)
 }
 
 func (p *Parser) findTitle(b byte) {
